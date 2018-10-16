20f7bc55f5d86614ecd30ff623810f78d82c6e1a
zhangjianguang <zhangjianguang@cmschina.com.cn>
2018-10-16 14:40:50 +0800

    fix bug

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
     std::cout << " hello git " << std::endl;
 
-    std::coutn << " print input args : " << std::endl;
+    std::cout << " print input args : " << std::endl;
 
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << std::endl;

057a4c7c608894422708b8f0511f822dec24f0d7
zhangjianguang <zhangjianguang@cmschina.com.cn>
2018-10-16 14:28:27 +0800

    add args

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-int main(int args[]) {
+int main(int argc, char* argv[]) {
     std::cout << " hello git " << std::endl;
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
 
     return 1;
 }
